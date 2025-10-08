3dc97d4e30366b760489279d0f3b8fc6efee4e37
Dali-Math <momo18669@gmail.com>
2025-10-08 09:10:18 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main className="p-4 sm:p-6 lg:p-8 max-w-5xl mx-auto space-y-6">
+      <h1 className="text-2xl font-semibold">VernierSim</h1>
+      <p className="text-base">Application en cours de développement</p>
+    </main>
+  );
+}
